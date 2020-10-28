9eb03cae3604cbaa028637bdcfe3a06f10baa550
Scheyk <kathrin.dostal@gmail.com>
2020-10-28 09:55:08 +0100

    create my class with a print method

---
+++ CsharpDay2/Kathi/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpDay2.Kathi
+{
+    class ClassOne
+    {
+        public void printName()
+        {
+            Console.WriteLine("print Name :)");
+        }
+    }
+}
