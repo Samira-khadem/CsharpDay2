ec24ce5ad3fcf8c1dd3081a5ddbfe4e7197a6ce8
samira khadem <samira.khademhoseini@gmail.com>
2020-10-28 09:31:27 +0100

    Add project files.

---
+++ CsharpDay2/Program.cs
@@ -0,0 +1,32 @@
+﻿using CsharpDay2.Samira;
+using System;
+
+namespace CsharpDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SamiraTask();
+            KathiTask();
+            CornelTask();
+
+            
+            Console.WriteLine("Hello World!");
+        }
+        static void SamiraTask()
+        {
+           
+            Tasks newtask = new Samira.Tasks();
+            newtask.TaskOne();
+        }
+        static void KathiTask()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void CornelTask()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CsharpDay2/Samira/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpDay2.Samira
+{
+    class Tasks
+    {
+        public void TaskOne()
+        {
+            Console.WriteLine("task one");
+        }
+    }
+}
